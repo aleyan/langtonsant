53d783f294e871465c48da6ca7f496fcdef7ce99
Alexander Yankov <yankov.alex@gmail.com>
2018-03-25 21:10:52 -0400

    Broke drawing into the canvas Struct & Impl

--- src/main.rs
+++ src/main.rs
@@ -9,46 +9,50 @@ use std::io::{Write, stdout};
 use std::cmp::{min, max};
 use std::{thread, time};
 
-fn main() {
-    let stdout = stdout();
-    let mut stdout = stdout.lock().into_raw_mode().unwrap();
-    let size = terminal_size().unwrap();
-    let columns = size.0 as i32;
-    let rows = (size.1 - 1) as i32;
-    write!(stdout, "{}{}{}{}",
-        clear::All,
-        cursor::Hide,
-        color::Bg(color::White),
-        color::Fg(color::Black),
-        ).unwrap();
-    for row in 1..(rows+1){
-        write!(stdout, "{}{}",
-            cursor::Goto(1,row as u16),
-            " ".repeat(columns as usize)).unwrap();
+pub(crate) struct Canvas {
+    columns: i32,
+    rows: i32,
+}
+
+impl Canvas {
+    pub(crate) fn new() -> Self {
+        let size = terminal_size().unwrap();
+        let columns = size.0 as i32;
+        let rows = (size.1 - 1) as i32;
+
+        let stdout = stdout();
+        let mut stdout = stdout.lock().into_raw_mode().unwrap();
+        write!(stdout, "{}{}{}{}",
+            clear::All,
+            cursor::Hide,
+            color::Bg(color::White),
+            color::Fg(color::Black),
+            ).unwrap();
+        for row in 1..(rows+1){
+            write!(stdout, "{}{}",
+                cursor::Goto(1,row as u16),
+                " ".repeat(columns as usize)).unwrap();
+        }
+        stdout.flush().unwrap();
+        Canvas {
+            columns: columns,
+            rows: rows,
+        }
     }
-    stdout.flush().unwrap();
 
-    let white: Complex<i32> = Complex::new(0, -1);
+    pub(crate) fn draw(&self, board: &HashMap<Complex<i32>, Complex<i32>>, 
+        ant_position: &Complex<i32>){
+        let stdout = stdout();
+        let mut stdout = stdout.lock().into_raw_mode().unwrap();
+        let white: Complex<i32> = Complex::new(0, -1);
 
-    let mut board: HashMap<Complex<i32>, Complex<i32>> = HashMap::new();
-    let mut ant_position: Complex<i32> = Complex::new(0, 0);
-    let mut ant_direction: Complex<i32> = Complex::new(-1, 0);
-
-    for _ in 0..15000 {
-        let square_color = board.get(&ant_position).cloned().unwrap_or(white);
-        ant_direction =  ant_direction * square_color;
-        board.insert(ant_position, -square_color);
-        ant_position = ant_position + ant_direction;
-
-        //draw(&board);
-        // draw begins here
-        let (ant_column, ant_row) = complex_to_screen(rows, columns, ant_position);
-        for row in max(1, ant_row-1)..min(ant_row + 1, rows + 1) {
+        let (ant_column, ant_row) = self.complex_to_screen(*ant_position);
+        for row in max(1, ant_row-1)..min(ant_row + 1, self.rows + 1) {
             write!(stdout, "{}",
                 cursor::Goto(max(1, ant_column - 1) as u16, row as u16),
                 ).unwrap();
-            for column in max(1, ant_column - 1)..min(ant_column + 1, columns + 1) {
-                let (top, bottom) = screen_to_complex(rows, columns, row, column);
+            for column in max(1, ant_column - 1)..min(ant_column + 1, self.columns + 1) {
+                let (top, bottom) = self.screen_to_complex(row, column);
                 let top_color = board.get(&top).cloned().unwrap_or(white);
                 let bottom_color = board.get(&bottom).cloned().unwrap_or(white);
                 let symbol = if top_color == white && bottom_color == white {
@@ -68,32 +72,52 @@ fn main() {
         stdout.flush().unwrap();
         thread::sleep(time::Duration::from_millis(1));
     }
-    
-    write!(stdout, "{}{}{}",
-        cursor::Goto(1, (rows + 1) as u16),
-        style::Reset,
-        cursor::Show
-        ).unwrap();
-}
 
-fn screen_to_complex(rows: i32, columns: i32, row: i32, column: i32) 
-    -> (Complex<i32>, Complex<i32>) {
-    let x = column - columns / 2;
-    let y = rows - row * 2;
-    let top: Complex<i32> = Complex::new(x, y);
-    let bottom: Complex<i32> = Complex::new(x, y - 1);
-    (top, bottom)
-}
+    pub(crate) fn screen_to_complex(&self, row: i32, column: i32) 
+        -> (Complex<i32>, Complex<i32>) {
+        let x = column - self.columns / 2;
+        let y = self.rows - row * 2;
+        let top: Complex<i32> = Complex::new(x, y);
+        let bottom: Complex<i32> = Complex::new(x, y - 1);
+        (top, bottom)
+    }
 
-fn complex_to_screen(rows: i32, columns: i32, loc: Complex<i32>) -> (i32, i32) {
-    let column = loc.re + columns / 2;
-    let row = (-loc.im + rows) / 2;
-    (column,row)
-}
+    pub(crate) fn complex_to_screen(&self, loc: Complex<i32>) -> (i32, i32) {
+        let column = loc.re + self.columns / 2;
+        let row = (-loc.im + self.rows) / 2;
+        (column,row)
+    }
 
-//fn draw(board: &HashMap<Complex<i32>, Complex<i32>>) {
-//    let size = terminal_size().unwrap();
-//    let rows = size.0 - 1;
-//    let columns = size.1;
-//    println!("{:?} {}", size, board.len());
-//}
+    pub(crate) fn close(&self){
+        let stdout = stdout();
+        let mut stdout = stdout.lock().into_raw_mode().unwrap();
+        write!(stdout, "{}{}{}",
+            cursor::Goto(1, (self.rows + 1) as u16),
+            style::Reset,
+            cursor::Show
+            ).unwrap();
+    }
+ }
+
+
+fn main() {
+    let canvas = Canvas::new();
+
+    let white: Complex<i32> = Complex::new(0, -1);
+
+    let mut board: HashMap<Complex<i32>, Complex<i32>> = HashMap::new();
+    let mut ant_position: Complex<i32> = Complex::new(0, 0);
+    let mut ant_direction: Complex<i32> = Complex::new(-1, 0);
+
+    for _ in 0..15000 {
+        let square_color = board.get(&ant_position).cloned().unwrap_or(white);
+        ant_direction =  ant_direction * square_color;
+        board.insert(ant_position, -square_color);
+        ant_position = ant_position + ant_direction;
+
+        canvas.draw(&board, &ant_position);
+
+    }
+    canvas.close();
+
+}
