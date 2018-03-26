71c0e83ce63851b1dd5d195cd9dfd9fae5af399f
Alexander Yankov <yankov.alex@gmail.com>
2018-03-25 23:44:42 -0400

    Write to term only after ant has left the square.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::io::{Write, stdout};
-use std::cmp::{min, max};
 use std::{thread, time};
 
 use termion::{terminal_size, style, clear, cursor, color};
@@ -39,39 +38,55 @@ impl Canvas {
     }
 
     pub(crate) fn draw(&self, board: &HashMap<Complex<i32>, Complex<i32>>, 
-        ant_position: &Complex<i32>){
+        ant_position: &Complex<i32>, ant_direction: &Complex<i32>){
         let stdout = stdout();
         let mut stdout = stdout.lock().into_raw_mode().unwrap();
         let white: Complex<i32> = Complex::new(0, -1);
 
         let (ant_column, ant_row) = self.complex_to_screen(*ant_position);
-        for row in max(1, ant_row-1)..min(ant_row + 1, self.rows + 1) {
-            write!(stdout, "{}",
-                cursor::Goto(max(1, ant_column - 1) as u16, row as u16),
+        let (prev_column, prev_row) = self.complex_to_screen(
+            *ant_position - *ant_direction);
+
+        if (prev_column, prev_row) != (ant_column, ant_row)
+            && (1 <= prev_column) && (prev_column <= self.columns)
+            && (1 <= prev_row) && (prev_row <= self.rows) {
+            let (top, bottom) = self.screen_to_complex(
+                prev_column, prev_row);
+            let top_color = board.get(&top).cloned().unwrap_or(white);
+            let bottom_color = board.get(&bottom).cloned().unwrap_or(white);
+            let symbol = if top_color == white && bottom_color == white {
+                " "
+            } else if top_color != white && bottom_color == white {
+                "▀"
+            } else if top_color == white && bottom_color != white {
+                "▄"
+            } else if top_color != white && bottom_color != white {
+                "█"
+            }else {
+                "X"
+            };
+            write!(stdout, "{}{}{}{}",
+                cursor::Goto(prev_column as u16, prev_row as u16),
+                color::Bg(color::White),
+                color::Fg(color::Black),
+                symbol
                 ).unwrap();
-            for column in max(1, ant_column - 1)..min(ant_column + 1, self.columns + 1) {
-                let (top, bottom) = self.screen_to_complex(row, column);
-                let top_color = board.get(&top).cloned().unwrap_or(white);
-                let bottom_color = board.get(&bottom).cloned().unwrap_or(white);
-                let symbol = if top_color == white && bottom_color == white {
-                    " "
-                } else if top_color != white && bottom_color == white {
-                    "▀"
-                } else if top_color == white && bottom_color != white {
-                    "▄"
-                } else if top_color != white && bottom_color != white {
-                    "█"
-                }else {
-                    "X"
-                };
-                write!(stdout, "{}", symbol).unwrap();
-            }
         }
+
         stdout.flush().unwrap();
         thread::sleep(time::Duration::from_millis(1));
     }
+    pub(crate) fn close(&self){
+        let stdout = stdout();
+        let mut stdout = stdout.lock().into_raw_mode().unwrap();
+        write!(stdout, "{}{}{}",
+            cursor::Goto(1, (self.rows + 1) as u16),
+            style::Reset,
+            cursor::Show
+            ).unwrap();
+    }
 
-    fn screen_to_complex(&self, row: i32, column: i32) 
+    fn screen_to_complex(&self, column: i32, row: i32)
         -> (Complex<i32>, Complex<i32>) {
         let x = column - self.columns / 2;
         let y = self.rows - row * 2;
@@ -83,16 +98,6 @@ impl Canvas {
     fn complex_to_screen(&self, loc: Complex<i32>) -> (i32, i32) {
         let column = loc.re + self.columns / 2;
         let row = (-loc.im + self.rows) / 2;
-        (column,row)
-    }
-
-    pub(crate) fn close(&self){
-        let stdout = stdout();
-        let mut stdout = stdout.lock().into_raw_mode().unwrap();
-        write!(stdout, "{}{}{}",
-            cursor::Goto(1, (self.rows + 1) as u16),
-            style::Reset,
-            cursor::Show
-            ).unwrap();
+        (column, row)
     }
  }
