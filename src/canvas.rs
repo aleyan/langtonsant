53ef73fcf1045e08a7ee0ccef8c171e228302882
Alexander Yankov <yankov.alex@gmail.com>
2018-03-26 21:16:18 -0400

    Moved repeated logic out of draw into draw_cell.

--- src/canvas.rs
+++ src/canvas.rs
@@ -39,47 +39,20 @@ impl Canvas {
 
     pub fn draw(&self, board: &HashMap<Complex<i32>, Complex<i32>>, 
         ant_position: Complex<i32>, ant_direction: Complex<i32>){
-        let stdout = stdout();
-        let mut stdout = stdout.lock().into_raw_mode().unwrap();
-        let symbol = '▀';
-
         let cell_ant = self.complex_to_screen(ant_position);
-
-        // Draw over the cell ant has left
         let cell_prev = self.complex_to_screen(
             ant_position - ant_direction);
-        if cell_ant != cell_prev
-            && (1 <= cell_prev.0) && (cell_prev.0 <= self.columns)
-            && (1 <= cell_prev.1) && (cell_prev.1 <= self.rows) {
-            let (fg, bg): (&color::Color, &color::Color) = self.cell_colors(
-                cell_prev, ant_position, board);
-            write!(stdout, "{}{}{}{}",
-                cursor::Goto(cell_prev.0 as u16, cell_prev.1 as u16),
-                color::Fg(fg),
-                color::Bg(bg),
-                symbol
-                ).unwrap();
+
+        // Draw the cell with the ant
+        self.draw_cell(cell_ant, ant_position, board);
+        // Draw the cell where the ant was (to remove the red color)
+        if cell_ant != cell_prev {
+            self.draw_cell(cell_prev, ant_position, board);
         }
 
-        // Draw the ant
-        if (1 <= cell_ant.0) && (cell_ant.0 <= self.columns)
-            && (1 <= cell_ant.1) && (cell_ant.1 <= self.rows) {
-            let (fg, bg): (&color::Color, &color::Color) = self.cell_colors(
-                cell_ant, ant_position, board);
-            write!(stdout, "{}{}{}{}",
-                cursor::Goto(cell_ant.0 as u16, cell_ant.1 as u16),
-                color::Fg(fg),
-                color::Bg(bg),
-                symbol
-                ).unwrap();
-
-            thread::sleep(time::Duration::from_millis(0));
-        }
-
-
-
-        stdout.flush().unwrap();
+        thread::sleep(time::Duration::from_millis(0));
     }
+
     pub fn close(&self){
         let stdout = stdout();
         let mut stdout = stdout.lock().into_raw_mode().unwrap();
@@ -89,14 +62,30 @@ impl Canvas {
             cursor::Show
             ).unwrap();
     }
-    fn cell_colors(&self, cell_location: (i32, i32),
+
+    fn draw_cell(&self, cell_location: (i32, i32),
         ant_position: Complex<i32>,
-        board: &HashMap<Complex<i32>, Complex<i32>>) 
-        -> (&color::Color, &color::Color) {
+        board: &HashMap<Complex<i32>, Complex<i32>>) {
+        if !((1 <= cell_location.0) && (cell_location.0 <= self.columns)
+            && (1 <= cell_location.1) && (cell_location.1 <= self.rows)){
+            return
+        }
+
         let (top, bottom) = self.screen_to_complex(
             cell_location.0, cell_location.1);
-        (self.square_term_color(ant_position, top, &board),
-        self.square_term_color(ant_position, bottom, &board))
+        let (fg, bg): (&color::Color, &color::Color) = 
+            (self.square_term_color(ant_position, top, &board),
+            self.square_term_color(ant_position, bottom, &board));
+
+        let stdout = stdout();
+        let mut stdout = stdout.lock().into_raw_mode().unwrap();
+        write!(stdout, "{}{}{}{}",
+            cursor::Goto(cell_location.0 as u16, cell_location.1 as u16),
+            color::Fg(fg),
+            color::Bg(bg),
+            '▀'
+            ).unwrap();
+        stdout.flush().unwrap();
     }
 
     fn square_term_color(&self, ant_position: Complex<i32>,
